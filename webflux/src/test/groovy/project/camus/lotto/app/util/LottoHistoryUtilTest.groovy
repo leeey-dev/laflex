0e7dbafd60706c8b7dd1a8467d70b47c6af86691
euiyeol.lee <leeey.dev@gmail.com>
2023-12-16 16:51:10 +0900

    [camus-041] 2. webflux > lotto 수정

--- webflux/src/test/groovy/project/camus/lotto/app/util/LottoHistoryUtilTest.groovy
+++ webflux/src/test/groovy/project/camus/lotto/app/util/LottoHistoryUtilTest.groovy
@@ -1,6 +1,6 @@
 package project.camus.lotto.app.util
 
-import project.camus.reactive.api.lotto.util.LottoHistoryUtil
+import project.camus.webflux.api.lotto.util.LottoHistoryUtil
 import spock.lang.Specification
 
 class LottoHistoryUtilTest extends Specification {
